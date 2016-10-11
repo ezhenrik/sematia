5ba6d7a8bd0cec729a57af9049b40712d5d93fdb
Erik Henriksson <erik.henriksson@helsinki.fi>
2016-10-11 23:28:41 +0300

    no message

--- sematia/views/edit.py
+++ sematia/views/edit.py
@@ -82,16 +82,17 @@ def post_treebank():
 
             url = 'https://sosol.perseids.org/sosol/api/v1/publications/'+str(r_json['id'])
             headers = {
-                'Content-Type': 'application/json; charset=UTF-8',
+                'Content-Type': 'application/json',
                 'Accept': 'application/json',
                 'Authorization': 'Bearer '+access_token
             }
             data = {
                 'community_name': app.app.config['PERSEIDS_COMMUNITY_NAME']
             }
-            r = requests.put(url, headers=headers, data=data)
+            r = requests.post(url, headers=headers, data=data)
             Log.p(r.status_code)
             Log.p(r.text)
             Log.p(app.app.config['PERSEIDS_COMMUNITY_NAME'])
             if (r.status_code == '200'):
                 return 'ok'
+    return 'false'
