f4886e9412c9342373a1164c1337751b2d3804a3
Erik Henriksson <erik.henriksson@helsinki.fi>
2017-03-14 12:38:22 +0200

    Bug testing

--- sematia/utils/learn.py
+++ sematia/utils/learn.py
@@ -1,9 +1,9 @@
 from .. import app
 from . import log
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-from scipy.cluster.hierarchy import ward, dendrogram
+#from sklearn.feature_extraction.text import CountVectorizer
+#from sklearn.feature_extraction.text import TfidfVectorizer
+#from sklearn.metrics.pairwise import cosine_similarity
+#from scipy.cluster.hierarchy import ward, dendrogram
 #import matplotlib.pyplot as plt
 from io import BytesIO
 
