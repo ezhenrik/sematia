eb7ce815b0a340bfcd857274aaca1786ed89b179
Erik Henriksson <erik.henriksson@helsinki.fi>
2017-03-14 12:40:20 +0200

    Bug tracking

--- sematia/utils/learn.py
+++ sematia/utils/learn.py
@@ -1,6 +1,6 @@
 from .. import app
 from . import log
-from sklearn.feature_extraction.text import CountVectorizer
+#from sklearn.feature_extraction.text import CountVectorizer
 #from sklearn.feature_extraction.text import TfidfVectorizer
 #from sklearn.metrics.pairwise import cosine_similarity
 #from scipy.cluster.hierarchy import ward, dendrogram
