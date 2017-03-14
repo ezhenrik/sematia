897fa08a26dccc75f3928d0d0366fbbc7a0bd118
Erik Henriksson <erik.henriksson@helsinki.fi>
2017-03-14 13:02:29 +0200

    Bugfix

--- sematia/utils/learn.py
+++ sematia/utils/learn.py
@@ -4,8 +4,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy.cluster.hierarchy import ward, dendrogram
-import matplotlib.pyplot as plt
-#import matplotlib as mpl
+#import matplotlib.pyplot as plt
+import matplotlib as mpl
 print(mpl.get_cachedir())
 from io import BytesIO
 
