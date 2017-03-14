cf3ff2240f3fd49f83728431e4649e6c1f139e59
Erik Henriksson <erik.henriksson@helsinki.fi>
2017-03-14 13:52:18 +0200

    no message

--- sematia/utils/learn.py
+++ sematia/utils/learn.py
@@ -1,9 +1,9 @@
 from .. import app
 from . import log
-#from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 import matplotlib.pyplot as plt
-from sklearn.feature_extraction.text import CountVectorizer
+#from sklearn.feature_extraction.text import CountVectorizer
 
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy.cluster.hierarchy import ward, dendrogram
