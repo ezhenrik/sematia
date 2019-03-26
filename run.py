784b5b1eb9161bde31aed5c89ceb5045d35f42bb
Erik Henriksson <erik.henriksson@helsinki.fi>
2019-03-25 19:27:29 -0700

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 from config import VENV_PATH
 
-sys.path.append(VENV_PATH + '/lib/python3.5/site-packages')
+sys.path.append(VENV_PATH + '/lib/python3.7/site-packages')
 
 from sematia import app
 
